1f240a8af5557c87b5fc07421e453d3fe1d1f33d
Bennett Smrdel <smrdelb@gmail.com>
2021-12-15 16:03:46 -0600

    secrets only show based on user's clearence level

--- server/routes/secrets.router.js
+++ server/routes/secrets.router.js
@@ -8,7 +8,11 @@ router.get('/', rejectUnauthenticated, (req, res) => {
   console.log('req.user:', req.user);
 
   pool
-    .query(`SELECT * FROM "secret";`)
+    const sqlText = `
+    SELECT * FROM "secret"
+    WHERE "secrecy_level"<=$1;
+    `;
+    pool.query(sqlText, [req.user.clearance_level])
     .then((results) => res.send(results.rows))
     .catch((error) => {
       console.log('Error making SELECT for secrets:', error);
